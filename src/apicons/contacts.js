bb27df2905c1289ce5b2d0a081088d7bd9d5220e
Ida <ida.czarniecka@gmail.com>
2020-01-26 15:26:12 +0100

    po 26.01a

--- src/apicons/contacts.js
+++ src/apicons/contacts.js
@@ -9,7 +9,9 @@ const Contacts = ({contacts}) => {
                     <div className="card-body">
                         <h5 className="card-title">{contact.name}</h5>
                         <h6 className="card-subtitle mb-2 text-muted">{contact.email}</h6>
+                        <h6 className="card-subtitle mb-2 text-muted">{contact.website}</h6>
                         <p className="card-text">{contact.company.catchPhrase}</p>
+                        <p className="card-text">{contact.address.street}</p>
                     </div>
                 </div>
             ))}
